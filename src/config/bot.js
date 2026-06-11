ad4a5fc604f0566a6a8494e3a6247fbfc9d02e66
AyushOP69 <132183075+AyushOP69@users.noreply.github.com>
2026-06-11 20:58:24 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "https://discord.gg/GWHJHS8ec2",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
